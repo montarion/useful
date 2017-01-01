897b75818730264734e1c7424cd29e24f86a450a
Jamiro <jamirograntsaan@gmail.com>
2017-01-01 22:44:03 +0100

    adding final Py2Exe program, done with this one for now :)

--- py2exe/start.py
+++ py2exe/start.py
@@ -7,18 +7,19 @@ from components.py2exe import py2exe
 #---url==link to pyinstaller---#
 url = "https://github.com/pyinstaller/pyinstaller/releases/download/v3.2/PyInstaller-3.2.zip"
 #---req check---#
-filename = input("what is the file that you want to convert?")
 if not os.path.exists(os.path.join(os.getcwd(), "input")):
+    print("creating folders..")
     init = firstlaunch(url)
 
 if not os.path.exists(os.path.join(os.getcwd(), "tools", "PyInstaller-3.2")):
     firstlaunch(url).download()
 
+filename = input("what is the file that you want to convert?")
 if not os.path.isfile(os.path.join(os.getcwd(), "input", filename)):
     print("please place", filename, "in the input folder and run the program again.")
     sleep(3)
     os.system("explorer " + os.path.join(os.getcwd(), "input"))
-    exit()
+
 else:
     install = py2exe(filename)
     install.installing()

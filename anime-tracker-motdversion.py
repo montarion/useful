a41e9d63490a91841a4de00eb5de2da098699ae8
Jamiro <jamirograntsaan@gmail.com>
2017-02-17 18:46:25 +0100

    cleaned it up a bit

---
+++ anime-tracker-motdversion.py
@@ -0,0 +1,24 @@
+import json
+
+import requests
+def search():
+    #---url for releases, in JSON---#
+    url = 'http://www.masterani.me/api/releases'
+
+    #---needed conversion---#
+    test = requests.get(url)
+    test1 = test.text
+    test = str(test1)
+    string = json.loads(str(test))
+    anime0 = string[0]['anime']['title']
+    anime1 = string[1]['anime']['title']
+    anime2 = string[2]['anime']['title']
+    anime3 = string[3]['anime']['title']
+    anime4 = string[4]['anime']['title']
+
+    print(anime0)
+    print(anime1)
+    print(anime2)
+    print(anime3)
+    print(anime4)
+search()

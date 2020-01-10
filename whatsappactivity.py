d7f349a99597d8de608f86193c8367cd796193bc
montarion <jamirograntsaan@gmail.com>
2020-01-11 00:17:26 +0100

    updated whatsapp activity tracker

--- whatsappactivity.py
+++ whatsappactivity.py
@@ -1,14 +1,17 @@
+
 import os, re, datetime
 from time import sleep
-
+#### MAKE IT LOOK STARTING FROM X DAYS AGO, 30 SHOULD BE FINE
 ###Variables!
 names = []
 dates = []
-datedict = {}
 late = []
 latedict = {}
-fakereq = 15
+
+fakereq = 14 
+tht = -50
 requirement = int(- + fakereq)
+blacklist = ["names", "you", "don't", "want"]
 
 ###Colours
 OKGREEN = '\033[92m'
@@ -20,14 +23,14 @@ ENDC = '\033[0m'
 ####Start
 currentdate = datetime.datetime.now().strftime("%d/%m/%y")
 #GET FRESH CHAT FILE FROM EMAIL FIRST
-#input('please press enter to continue')
-infile = open('chat.txt', encoding='utf-8')
+print("make sure you have a whatsapp group \"chat.txt\" file in a folder called \"waplogs\"")
+input('please press enter to continue')
+infile = open('waplogs/chat.txt', encoding='utf-8')
 badchat = infile.readlines()
 badchat.reverse()
 infile.close()
-outfile = open('chat2.txt', 'w', encoding="utf-8")
+outfile = open('waplogs/chat2.txt', 'w', encoding="utf-8")
 for line in badchat:
-
     outfile.write(line)
 outfile.close()
 
@@ -39,7 +42,16 @@ def lastseen(date):
     inactivity = int(round(difference / datetime.timedelta(days=1)))
     return inactivity
 print(WARNING + "requirement is for users to have said something in the last {} days.".format(fakereq))
-with open('chat2.txt', encoding="utf-8") as chat:
+sleep(2)
+print("3...")
+sleep(1)
+print("2..")
+sleep(0.8)
+print("1.")
+sleep(0.6)
+print("let's  go.")
+sleep(0.5)
+with open('waplogs/chat2.txt', encoding="utf-8") as chat:
     for line in chat:
         #sleep(0.0001)
 
@@ -50,59 +62,49 @@ with open('chat2.txt', encoding="utf-8") as chat:
             result = re.search(search, line)
             name = result.group(3)
             date = result.group(1)
-            if "changed the subject from" not in name and name not in names:
-                #print(WARNING + name + " " + date + ENDC)
+            if "changed the subject from" not in name and name not in names and name not in blacklist:
+                #print(WARNING + "processing message from: " + name + " " + date + ENDC)
                 #print(OKGREEN + str(names) + ENDC)
-                sleep(0.1)
+                sleep(0.00001)
                 if name[0] == '+':
-                    if name[1:-1] not in names:
-                        if lastseen(date) < requirement:
-                            late.append(name[1:-1])
-                            names.append(name[1:-1])
-                            latedict.update({name: date})
-
-                            #print(RED + 'processing ' + name[1:-1] + ENDC)
-
-                            #print(OKBLUE + str(name) + ENDC)
-
+                    name = name[1:]
+                    if name not in names:
+                        inactivity = lastseen(date)
+                        if inactivity < requirement and inactivity > tht:
+                            late.append(name)
+                            names.append(name)
+                            latedict.update({name: [date, inactivity]})
+                            print(OKBLUE + 'processing ' + name + ENDC)
+                            print(OKBLUE + str(name) + ENDC)
                         else:
-                            #print('adding {}'.format(name[1:-1]))
-                            names.append(name[1:-1])
-                            datedict.update({name[1:-1]:date})
-                            #print(OKGREEN + name + ENDC)
-                            #print(OKGREEN + 'processing ' + name[1:-1] + ENDC)
-
-
+                            print(OKGREEN + 'adding {}'.format(name) + ENDC)
+                            names.append(name)
+                            print(OKBLUE + 'processing ' + name + ENDC)
                 else:
-
-                    if lastseen(date) < requirement:
-                        late.append(name)
-                        names.append(name)
-                        latedict.update({name:date})
-                        #print(RED + 'processing ' + name + ENDC)
-
-                    else:
-                        names.append(name)
-                        datedict.update({name:date})
-                        #print(OKGREEN + 'processing ' + name + ENDC)
-
+                    if name not in names:
+                        inactivity = lastseen(date)
+                        if inactivity < requirement and inactivity > tht:
+                            late.append(name)
+                            names.append(name)
+                            latedict.update({name:[date, inactivity]})
+                            print(OKBLUE + 'processing ' + name + ENDC)
+                        else:
+                            names.append(name)
+                            print(OKBLUE + 'processing ' + name + ENDC)
         except AttributeError:
             continue
 
 
-
-#for line in late:
-
-#    final = RED + line + ' was last active at: ' + latedict[line] + ENDC
-#    print(final)
-names.sort()
-#print(names)
-
-for line in names:
-    if line not in latedict:
-        print(OKGREEN + line + OKBLUE + ' was last active at: ' + OKGREEN + datedict[line] + ENDC)
-    
-    else:
-        print(RED + line + OKBLUE + ' was last active at: ' + RED + latedict[line] + ENDC)
+superfinallatelist = []
+for line in late:
+    try:
+        final = RED + "{} has last been active at: {}, {} days ago.".format(line, latedict[line][0], str(latedict[line][1])[1:]) + ENDC
+        print(final)
+        latefile = open('latepeeps.txt', 'a')
+        latefile.write("@{} ".format(line))
+        latefile.close()
+    except Exception as e:
+        print(e)
+        print("couldn't process: {}".format(line))
 
 
